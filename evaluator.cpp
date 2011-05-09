019506ab131b73a59353b0cfc8f5e2da999cdd3d
Alexandre Moreira <alexandreamn@gmail.com>
2011-05-09 18:02:50 -0300

    Created manual eval dispatcher

---
+++ evaluator.cpp
@@ -0,0 +1,14 @@
+#include <error.h>
+
+#include "evaluator.h"
+#include "reader.h"
+#include "special.h"
+
+
+value_t evaluate(value_t expr) {
+	value_t result = UNSPECIFIED;
+	error(1, 0, "Could not evaluate value 0x%016lX\n: Unknown type." expr);
+	return result;
+}
+
+
---
+++ evaluator.h
@@ -0,0 +1,8 @@
+#ifndef __EVALUATOR_H__
+#define __EVALUATOR_H__
+
+#include "value.h"
+
+value_t evaluate(value_t);
+
+#endif
