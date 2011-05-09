f281d73fd19649b9afabb9c86a71ea7d4b56abea
Alexandre Moreira <alexandreamn@gmail.com>
2011-05-09 18:20:56 -0300

    Created printer for booleans

--- printer.cpp
+++ printer.cpp
@@ -3,9 +3,19 @@
 #include <error.h>
 
 #include "printer.h"
+#include "special.h"
 
 void print(value_t value) {
-	error(1, 0, "Unable to print! Unknown value: 0x%016lX\n", value);
+	std::string result;
+
+	// TODO: This must be a generic function. Not this mess.
+	if (is_boolean(value)) {
+		result = boolean_format(value);
+	}
+	else {
+		error(1, 0, "Unable to print! Unknown value: 0x%016lX\n", value);
+	}
+	std::cout << result << std::endl;
 }
 
 
