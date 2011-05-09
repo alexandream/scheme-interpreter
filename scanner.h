a4031dd3dde858850680ce6afebe21674f43f8cc
Alexandre Moreira <alexandreamn@gmail.com>
2011-05-09 16:48:28 -0300

    Created lexer interface. get_token and peek_token functions

---
+++ scanner.h
@@ -0,0 +1,27 @@
+#ifndef __SCANNER_H__
+#define __SCANNER_H__
+
+#include <string>
+
+enum token_types_t {
+   TK_NONE,
+   TK_EOF,
+  
+   TK_LPAREN,
+   TK_RPAREN,
+   TK_INTEGER,
+   TK_SYMBOL,
+   TK_BOOLEAN,
+
+   TK_DOT
+};
+
+struct token_t {
+    token_types_t type;
+	std::string lexeme;
+};
+
+token_t peek_token();
+token_t get_token();
+#endif
+
