87672a06b02e5caf39a264232eab406aaad8f2f3
Alexandre Moreira <alexandreamn@gmail.com>
2011-09-14 12:30:37 -0300

    Created internal type predicate "is_environment".

--- environment.h
+++ environment.h
@@ -19,6 +19,11 @@ value_t make_environment(value_t parent,
                          value_t values = EMPTY_LIST);
 
 
+static inline
+bool is_environment(value_t value) {
+	return (!is_immediate(value) &&
+	        get_non_immediate_type(value) == ENVIRONMENT_TYPE_MASK);
+}
 value_t environment_get(value_t env, value_t symbol);
 void environment_set(value_t env, value_t symbol, value_t value);
 void environment_add(value_t env, value_t symbol, value_t value);
